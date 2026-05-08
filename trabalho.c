215e8ae3afafd00f54463885d0c133129175692c
hgiulliano <henrique.giulliano@gmail.com>
2026-05-08 12:45:50 -0300

    Adicionei duas funcoes, uma pra criar e uma pra mostrar o tabuleiro.

--- trabalho.c
+++ trabalho.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+void criarTabuleiro(char tabuleiro[8][8]){
+    for (int i=0; i<8; i++){
+        for (int j=0; j<8; j++){
+            tabuleiro[i][j] = '0';
+        }
+    }
+}
+void mostrarTabuleiro(char tabuleiro[8][8]){
+    for (int i=0; i<8; i++){
+        for (int j=0; j<8; j++){
+            printf("%c ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void main(){
+    char tabuleiro[8][8];
+
+    criarTabuleiro(tabuleiro);
+    mostrarTabuleiro(tabuleiro);
     printf("Hello World");
 }
