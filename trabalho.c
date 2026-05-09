28383e44c12d79c6cf3159f593e034b547cb0327
hgiulliano <henrique.giulliano@gmail.com>
2026-05-09 18:35:04 -0300

    Duas funcoes novas, uma  pra escolher a peca e outra pra  encaixar a peca.

--- trabalho.c
+++ trabalho.c
@@ -1,25 +1,69 @@
 #include <stdio.h>
 
-void criarTabuleiro(char tabuleiro[8][8]){
-    for (int i=0; i<8; i++){
-        for (int j=0; j<8; j++){
-            tabuleiro[i][j] = '0';
+void criarTabuleiro(char tabuleiro[10][10]){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
+            tabuleiro[i][j] = ' ';
         }
     }
 }
-void mostrarTabuleiro(char tabuleiro[8][8]){
-    for (int i=0; i<8; i++){
-        for (int j=0; j<8; j++){
+void mostrarTabuleiro(char tabuleiro[10][10]){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<10; j++){
             printf("%c ",tabuleiro[i][j]);
         }
         printf("\n");
     }
 }
+void escolherPeca(int tipoPeca, char peca[3][3]){
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            peca[i][j] = ' ';
+        }
+    }
+    switch(tipoPeca){
+        case 1:
+            peca[0][0] = '1'; peca[0][1] = '1'; peca[0][2] = '1';
+            peca[1][0] = '1'; peca[1][1] = '1'; peca[1][2] = '1';
+            peca[2][0] = '1'; peca[2][1] = '1'; peca[2][2] = '1';
+            break;
+        case 2:
+            peca[0][0] = '1';
+            peca[1][0] = '1'; peca[1][1] = '1';
+            break;
+        case 3:
+            peca[0][0] = '1'; peca[0][1] = '1';
+            break;
+        case 4:
+            peca[0][0] = '1';
+            peca[1][0] = '1';
+            break;
+        case 5:
+            peca[0][0] = '1';
+            break;
+        }
+}
+void encaixarPeca(char tabuleiro[10][10],char peca[3][3], int linha, int coluna){
+    for (int i = 0; i<3;i++){
+        for (int j = 0; j<3;j++){
+            if (peca[i][j]!= ' '){
+                tabuleiro[linha+i][coluna+j] = peca[i][j];
+            }
+        }
+    }
+}
 
 void main(){
-    char tabuleiro[8][8];
-
+    char tabuleiro[10][10], peca[3][3];
+    
+    printf("Wood Blocks Console Game -- Trabalho laboratorio de logica de programacao");
+    printf("\n \n");
     criarTabuleiro(tabuleiro);
+    escolherPeca(1,peca);
+    encaixarPeca(tabuleiro,peca,5,6);
+    escolherPeca(1,peca);
+    encaixarPeca(tabuleiro,peca,2,3);
+    escolherPeca(2,peca);
+    encaixarPeca(tabuleiro,peca,1,7);
     mostrarTabuleiro(tabuleiro);
-    printf("Hello World");
 }
